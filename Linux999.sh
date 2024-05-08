ea0351830918da39b3b7b63e746fad44c44078e5
Hengker12345 <167304514+Hengker12345@users.noreply.github.com>
2024-05-08 23:28:44 +0700

    Linux999.sh

---
+++ Linux999.sh
@@ -0,0 +1,188 @@
+#Linux999
+#Debian
+
+sleep 1
+clear
+sleep 1
+echo -e "      ⠀⠀⠀⠀⠀⠀⠀⠀⠀              ⠀⣀⣤⣤⣤⣤⣤⣤⣀⡀"⠀⠀⠀⠀⠀⠀⠀
+echo -e " ⠀  ⠀⠀   ⠀⠀⠀             ⢀⣠⣶⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣷⣦⣄⣀⣤⣶⣶"
+echo -e " ⠀⠀⠀  ⠀                ⠀⣰⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⣿⠟⠋"
+echo -e " ⠀⠀⠀    ⠀              ⣼⣿⡿⠃⠀⢀⣤⣾⣿⣿⣿⣿⣷⣦⣄⠀⠀⠈⠉   "⠀⠀⠀
+echo -e " ⠀  ⠀                ⠀⣸⣿⡿⠁⠀⢠⣿⣿⠟⠉⠀⠈⠉⠛⢿⣿⣷⡄⠀⠀⠀⠀⠀"
+echo -e "      ⠀⠀             ⢀⣿⣿⡇⠀⠀⣾⣿⡟⠀⠀⢀⣤⣄⠀⠀⠹⣿⣿⡄⠀⠀⠀⠀"
+echo -e " ⠀⠀                  ⣾⣿⣿⡇⠀⠀⢻⣿⣷⡀⠀⠘⣿⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀"
+echo -e "                    ⣼⣿⡿⣿⣿⡄⠀⠈⠻⣿⣿⣷⣿⣿⡿⠃⠀⢀⣿⣿⡇⠀⠀⠀⠀"
+echo -e "                   ⣰⣿⣿⠁⠹⣿⣿⣦⡀⠀⠈⠉⠛⠋⠉⠀⠀⣠⣾⣿⡟⠀⠀⠀⠀⠀"
+echo -e "                   ⣿⣿⣧⣤⣤⣬⣿⣿⣿⣶⣦⣤⣤⣤⣴⣶⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀"
+echo -e "                   ⠙⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀"
+echo -e
+echo -e "           ________________________________________"
+echo -e "          |                                        |"
+echo -e "          |1.DDOS                7.BOT WA          |"
+echo -e "          |2.SPAM                8.NETHUNTER M     |"
+echo -e "          |3.BANED WA            9.TERMUX LXQT     |"
+echo -e "          |4.UNBANED WA          10.ELITE2         |"
+echo -e "          |5.HACK SOSMED         11.VIPER          |"
+echo -e "          |6.SPLOIT-MALICIOUS    12.SPAMM          |"
+echo -e "          |13.DDOS R             14.Tools999       |"
+echo -e "          |                                        |"
+echo -e "          |________________________________________|"
+read -p "          PILIH MENU:" HOOH
+if [ $HOOH == 1 ]
+then
+clear
+sleep 1
+sudo apt-get update && sudo apt-get updgrade
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install curl
+sudo apt install git
+git clone https://github.com/Whomrx666/anonymous-c2
+cd anonymous-c2
+pip3 install -r requirements.txt
+python3 anonymous-c2.py
+fi
+
+if [ $HOOH == 2 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install  git
+ python -m pip install requests
+ git clone https://github.com/RozhBasXYZ/OTP
+ cd OTP
+ python spam.py
+fi
+
+if [ $HOOH == 3 ]
+then
+clear
+sleep 1
+xdg-open "https://drive.google.com/file/d/152fTXoG9IEsp1unLH47Ap1mV3AvpsYYX/view?usp=drivesdk"
+fi
+
+if [ $HOOH == 4 ]
+then
+clear
+sleep 1
+xdg-open "https://drive.google.com/file/d/14V3b11fDLTNzddSKTdVZiNlDwK7JBTjD/view?usp=drivesdk"
+fi
+
+if [ $HOOH == 5 ]
+then
+clear
+sleep 1
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+cd zphisher
+bash zphisher.sh
+fi
+
+if [ $HOOH == 6 ]
+then
+clear
+sleep 1
+apt-get install git
+apt-get install python2 ruby
+gem install lolcat
+git clone https://github.com/TheSploit/Sploit-Malicious
+cd Sploit-Malicious
+pip2 install -r requirements.txt
+python2 malicious.py
+fi
+
+if [ $HOOH == 7 ]
+then
+clear
+sleep 1
+xdg-open "https://www.mediafire.com/file/f52nygbxa8kb580/node_modules.zip/file"
+fi
+
+if [ $HOOH == 8 ]
+then
+clear
+sleep 1
+pkg install root-repo 
+ pkg install x11-repo
+ apt update && apt upgrade -y
+ apt install wget -y 
+ wget https://raw.githubusercontent.com/shahinuralamin/Technical_Shahinur_Al-Amin/main/install-nethunter-modified 
+chmod +x install-nethunter-modified
+./install-nethunter-modified
+fi
+
+if [ $HOOH == 9 ]
+then
+clear
+sleep 1
+cd $HOME
+pkg update && pkg upgrade
+pkg install git
+git clone --depth 1 https://github.com/Yisus7u7/termux-desktop-lxqt 
+cd termux-desktop-lxqt 
+bash install-desktop-lxqt.sh
+start-desktop
+fi
+
+if [ $HOOH == 10 ]
+then
+clear
+sleep 1
+pkg update -y && pkg upgrade -y
+pkg install git python
+git clone --depth 1 https://github.com/Dapunta/elite2
+cd elite2
+git pull
+pip install -r requirements.txt
+python run.py
+fi
+
+if [ $HOOH == 11 ]
+then
+clear
+sleep 1
+apt update 
+apt upgrade 
+pkg install git 
+pkg install python
+git clone https://github.com/Moajjem404/VZERO
+cd VZERO
+pip install -r requirements.txt
+python main.py
+fi
+
+if [ $HOOH == 12 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install nano
+pkg install git
+pkg install python
+git clone https://github.com/AmmarrBN/Tools-WhatsApp
+bash install.sh
+cd Tools-WhatsApp
+python main.py
+fi
+
+if [ $HOOH == 13 ]
+then
+clear
+sleep 1
+pkg install git -y
+pkg install python -y
+pkg install python3 -y
+git clone https://github.com/palahsu/DDoS-Ripper.git
+cd DDoS-Ripper 
+ls
+python3 DRipper.py
+fi
+
+if [ $HOOH == 14 ]
+then
+clear
+sleep 1
+git clone https://github.com/SPOKYHOZOOO/TOOLS999 
+cd TOOLS999
+bash TOOLS999.sh
+fi
