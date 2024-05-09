5030b444bdaffeb88b6adfd423071bff9119f620
Hengker EpEp☕🗿 <167304514+Hengker12345@users.noreply.github.com>
2024-05-09 09:55:34 +0700

    Update Linux999.sh

--- Linux999.sh
+++ Linux999.sh
@@ -213,13 +213,3 @@ git clone https://github.com/MRHOOH/BLACKV1
 cd BLACKV1
 bash BLACKV1.sh
 fi
-
-if [ $HOOH == 17 ]
-then
-clear
-sleep 1
-Git clone https://github.com/HyukIsBack/KARMA-DDoS.git
-cd KARMA-DDoS
-pip install -r requirements.txt
-python3 main.py
-fi
