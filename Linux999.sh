ecb4a498704e42018caaecbe9d232dfd6ff27b0d
Hengker EpEp <167304514+Hengker12345@users.noreply.github.com>
2024-05-10 13:53:24 +0700

    Update Linux999.sh

--- Linux999.sh
+++ Linux999.sh
@@ -47,9 +47,9 @@ echo -e "          |31.bot WA             32.Followes tiktok|"
 echo -e "          |33.champhising        34.               |"
 echo -e "          |35.                   36.               |"
 echo -e "          |________________________________________|"
-echo -e "          |UPDATE SC TOOLS       |UPDATE           |"
-echo -e "          |FOLLOW AKUN TIKTOK KU |FOLLOW           |"
-echo -e "          |LAPORKAN BUG          |LAPORKAN         |"
+echo -e "          |UPDATE                                  |"
+echo -e "          |FOLLOW                                  |"
+echo -e "          |LAPORKAN                                |"
 echo -e "          |________________________________________|"
 echo
 read -p "          PILIH MENU:" HOOH
